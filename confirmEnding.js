27fc28c6747be7ef53781156be162f7c40340c4e
Hsien-Hsiu <hsien-hsiu.liao@yandex.com>
2020-08-25 18:03:28 -0700

    Update confirmEnding.js

--- confirmEnding.js
+++ confirmEnding.js
@@ -1,7 +1,16 @@
+/**
+ * @param {string} str
+ * @param {string} target
+ * @return {boolean}
+ */
+
+// compare end of word or phrase with given target
 function confirmEnding(str, target) {
-  // compare end of word or phrase with given target
   
   return str.substring(str.length,str.length-target.length)===target;
+
 }
 
+
 confirmEnding("He has to give me a new name", "name");
+// should return true
